730b253fd5b3313c57e1e30d0a60195b97c63b76
[A[Balexia turner <alexiaturner411@gmail.com>
2022-08-02 19:43:00 -0700

    can filter

--- index.js
+++ index.js
@@ -180,9 +180,17 @@ Use the filterByWord function below to do the following:
 */
 
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, flavor){
+ let filteredArray = [];
+ for(let i = 0; i < array.length; i++){
+  if(array[i].includes(flavor)){
+    filteredArray.push(array[i]);
+  }
+ }
+ return filteredArray;
 }
+
+console.log(filterByWord(originalFlavors, 'Chocolate'));
 
 
 
